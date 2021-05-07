4cfb67d20b011e74873a4caf0143f78168b5b19d
Archer Hsu <a0983770181@gmail.com>
2021-05-07 15:31:55 +0800

    建立 package

---
+++ note2.py
@@ -0,0 +1,5 @@
+#  1.在codenote1資料夾(git folder/project)內增加 code_note1資料夾(如果有空格 加入底線)
+#  package命名方式
+#  2.在裡面那個資料夾加入__init__.py為真正開發資料夾(package)
+#-------------任何有建立__init__.py的資料夾都是python package---------------------#
+#  真正可以被import的資料夾稱為package
