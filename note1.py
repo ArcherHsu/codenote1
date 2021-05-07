fa60ca20d10d2473ce7b762f3e0d8e04aae3ce56
Archer Hsu <a0983770181@gmail.com>
2021-05-07 15:12:36 +0800

    專案開始前置作業:包括 github連動, 建立venv, 建立.gitignore,  建立configuration(初始配置

---
+++ note1.py
@@ -0,0 +1,16 @@
+# 1  以製作 hololive 專案為例
+# ---------------------------------前置作業---------------------------------------#
+
+# 2  上github 創 hololive 專案  並且複製 "那6行"!
+# 3  桌面創hololive資料夾  稱為專案
+# 4  cmd叫出來 移至 專案資料夾 貼上"那六行" 並鍵入enter  完成 git連動
+# 5  cmd 鍵入 virtualenv venv 鍵入enter 製作好虛擬環境
+# 6  google python gitignore 並複製, 並且在專案製作 .gitignore (text檔改) sublime打開貼上儲存關閉
+# 7  按左上Add Configuration > + > python > 檔名:main > Python interpreter 選擇此專案模擬環境
+# 之 python(如果沒有找到 在 File > Settings > Python interpreter 裡抓
+# ctrl+k commit ctrl+shift+k push
+#--------------------------------前置作業完成--------------------------------------------#
+
+
+
+
